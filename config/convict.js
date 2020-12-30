02023edd586f99b6b7eed16b46408455bf6fa646
Mathieu Hicauber <mhicauber@okina.fr>
2020-12-30 15:25:16 +0100

    default auth realm name

--- config/convict.js
+++ config/convict.js
@@ -63,7 +63,7 @@ module.exports = new Promise(function (resolve, reject) {
     authRealmName: {
       doc: 'Authentication realm name',
       format: String,
-      default: '/Mobi-iti',
+      default: 'Mobi-iti',
       env: 'AUTH_REALM_NAME'
     },
     netexPrefix: {
