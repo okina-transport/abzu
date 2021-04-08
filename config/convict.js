3733ce0d10250179d9417ea1692042375d835cb8
gcarrere <gcarrere@okina.fr>
2021-04-08 16:12:22 +0200

    Fix default NETEX prefix

--- config/convict.js
+++ config/convict.js
@@ -70,6 +70,7 @@ module.exports = new Promise(function (resolve, reject) {
     netexPrefix: {
       doc: 'Netex Prefix to be used',
       format: String,
+      default: 'MOBIITI',
       env: 'NETEX_PREFIX'
     },
     mapboxAccessToken: {
