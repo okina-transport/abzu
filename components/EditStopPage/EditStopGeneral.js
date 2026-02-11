53c1671b3a220aac196713817e2ca2e8be12793c
poswald <poswald@okina.fr>
2026-02-11 11:52:02 +0100

    fix: DATA-1689 versions of KO breakpoints

--- components/EditStopPage/EditStopGeneral.js
+++ components/EditStopPage/EditStopGeneral.js
@@ -435,7 +435,7 @@ class EditStopGeneral extends React.Component {
       query: stopPlaceAndPathLinkByVersion,
       variables: {
         id,
-        version
+        version: parseInt(version)
       }
     });
   };
